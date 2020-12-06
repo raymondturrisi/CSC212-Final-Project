e33f013f8a70689c5c08c03f95f671fa381a1674
raymondturrisi <raymond.turrisi@gmail.com>
2020-12-06 04:32:09 -0500

    Good and working
    - inserts work
     - searches work (mostly)
     - overloaded operators work
     - nearest neighbors work
     - pre,post, and in order work
     - generating .dot files autonomously work if the graphviz package is installed locally (i.e. sudo apt-get install graphviz)
    Can begin to gather data
    
    Need to implement regional neighbors and range finders

--- project_v1/make.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Compiles with the following commands
-
-g++ main.cpp -ggdb -o prog.exe
-
-#/home/ubuntu/csc212/project/CSC212-Final-Project/data/breakdown_by_type/RI
--- project_v1/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-debug50 ./prog.exe /home/ubuntu/csc212/project/CSC212-Final-Project/data/breakdown_by_type/RI/coffee.csv
